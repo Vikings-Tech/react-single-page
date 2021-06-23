0460b4c8da9299602f3a7e0348a09df04bc86659
MysteriousAcadia <anshumansingh64@gmail.com>
2021-06-23 15:48:15 +0530

    Updates

---
+++ static/js/main.a5766e21.chunk.js
@@ -0,0 +1,19 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+* Copyright 2021 Creative Tim (http://www.creative-tim.com)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*! For license information please see main.a5766e21.chunk.js.LICENSE.txt */
+(this["webpackJsonppaper-kit-react"]=this["webpackJsonppaper-kit-react"]||[]).push([[0],{27:function(e,t,c){},28:function(e,t,c){},30:function(e,t,c){"use strict";c.r(t),t.default=c.p+"static/media/daniel-olahh.77c0ffac.jpg"},36:function(e,t,c){"use strict";c.r(t);var a=c(20),r=c(1),n=c.n(r),s=c(17),i=c.n(s),l=c(18),d=c(4),o=(c(26),c(27),c(28),c(37)),j=c(38),b=c(39),u=c(40),h=c(41),x=c(2);var f=function(){var e=n.a.createRef();return n.a.useEffect((function(){if(window.innerWidth<991){var t=function(){var t=window.pageYOffset/3;e.current.style.transform="translate3d(0,"+t+"px,0)"};return window.addEventListener("scroll",t),function(){window.removeEventListener("scroll",t)}}})),Object(x.jsx)(x.Fragment,{children:Object(x.jsxs)("div",{style:{backgroundImage:"url("+c(30).default+")"},className:"page-header","data-parallax":!0,ref:e,children:[Object(x.jsx)(o.a,{className:"ml-auto mr-auto",children:Object(x.jsxs)("div",{className:"motto text-center",style:{width:"100%"},children:[Object(x.jsx)("h1",{children:"Example page"}),Object(x.jsx)("h3",{children:"Start designing your landing page here."}),Object(x.jsx)("br",{}),Object(x.jsx)("br",{})]})}),Object(x.jsx)(o.a,{children:Object(x.jsxs)(j.a,{className:"card-register ml-auto mr-auto",children:[Object(x.jsx)("h3",{className:"title mx-auto",children:"Welcome"}),Object(x.jsxs)(b.a,{className:"register-form",children:[Object(x.jsx)("label",{children:"Field1"}),Object(x.jsx)(u.a,{placeholder:"Field1",type:"text"}),Object(x.jsx)("label",{children:"Field2"}),Object(x.jsx)(u.a,{placeholder:"Field2",type:"text"}),Object(x.jsx)(h.a,{block:!0,className:"btn-round",color:"danger",children:"Click me"})]})]})})]})})},m=c(42);var O=function(){return Object(x.jsx)("footer",{className:"footer footer-black footer-white",children:Object(x.jsx)(m.a,{children:Object(x.jsx)(o.a,{children:Object(x.jsxs)("span",{className:"copyright",style:{textAlign:"center",width:"100%"},children:["\xa9 ",(new Date).getFullYear(),", made with"," ",Object(x.jsx)("i",{className:"fa fa-heart heart"})," by Vikings"]})})})})};var p=function(){return document.documentElement.classList.remove("nav-open"),n.a.useEffect((function(){return document.body.classList.add("profile-page"),function(){document.body.classList.remove("profile-page")}})),Object(x.jsxs)(x.Fragment,{children:[Object(x.jsx)(f,{}),Object(x.jsx)(O,{})]})};i.a.render(Object(x.jsx)(l.a,{children:Object(x.jsxs)(d.d,{children:[Object(x.jsx)(d.b,{path:"/",render:function(e){return Object(x.jsx)(p,Object(a.a)({},e))}}),Object(x.jsx)(d.a,{to:"/index"})]})}),document.getElementById("root"))}},[[36,1,2]]]);
+//# sourceMappingURL=main.a5766e21.chunk.js.map
