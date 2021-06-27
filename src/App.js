4a905dbeaacd7991c9426728a51641256c73b123
akashjha011 <akash.jsmith@gmail.com>
2021-06-27 15:24:43 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {useState,useEffect} from "react";
+import Web3 from "web3";
+
+let web3,walletWeb3;
+function App() {
+
+  
+
+  useEffect(()=>{
+
+    
+  },[]);
+
+ 
+
+
+  return (
+    <div className="App">
+      <button type="button" className="button" onClick={onButtonClickMeta}>Submit</button>
+    </div >
+  );
+}
+
+export default App;
