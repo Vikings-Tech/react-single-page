c7c4f68ad077a524585e016606e144b09e44f798
akashjha011 <akash.jsmith@gmail.com>
2021-06-29 14:48:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,38 @@ let web3,contract;
 function App() {
 
   const [account,setAccount] = useState()
+  const contractAddress = "0xf9C40426513965AF10B722CE65e60D214966643A";
 
   useEffect(async()=>{
     if(!window.ethereum){
       console.log("Get metamask plis request");
       return;
     }
-    await window.ethereum.request({method:'eth_requestAccounts'});
-    web3 = new Web3(window.ethereum);
-    setAccount(window.ethereum.selectedAddress);
-    console.log(web3.eth.getChainId);
-    const contractAddress = "0xf9C40426513965AF10B722CE65e60D214966643A";
-    contract = new web3.eth.Contract(abi,contractAddress);
+    async function startMetamask(){
+      await window.ethereum.request({method:'eth_requestAccounts'});
+      web3 = new Web3(window.ethereum);
+      setAccount(window.ethereum.selectedAddress);
+      console.log(web3.eth.getChainId);
+      contract = new web3.eth.Contract(abi,contractAddress);
+      contract.getPastEvents("allEvents",
+    {                               
+        fromBlock: 15703788,     
+        // toBlock: END_BLOCK // You can also specify 'latest'          
+    })                              
+    .then(events => console.log(events))
+.catch((err) => console.error(err));
+    }
+    startMetamask();
     
   },[]);
-
-  contract.events.PayoutFulfilled()
-  .on('data', (event) => {
-	console.log(event);
-  })
-  .on('error', console.error);
-
+    if(contract != null){
+    contract.events.PayoutFulfilled()
+    .on('data', (event) => {
+    console.log(event);
+    })
+    .on('error', console.error);
+    console.log("working")
+  }
   async function Gamble(){
     const amount = web3.utils.toWei("0.05","ether");
     const result = contract.methods.gamble().send({from: account, value:amount});
